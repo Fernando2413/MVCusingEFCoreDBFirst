af5c4e007e7cd51bb44d850fe1146fed2bb50892
CARLOS FERNANDO HERNANDEZ LEON <carlos.hernandez241403@liveusam.edu.sv>
2025-03-22 08:12:48 -0600

    Configurar Program.cs y cadena de conexión.

--- MVCusingEFCoreDBFirst/Program.cs
+++ MVCusingEFCoreDBFirst/Program.cs
@@ -1,23 +1,39 @@
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+using Microsoft.EntityFrameworkCore;
+using MVCusingEFCoreDBFirst.Models;
+namespace MVCusingEFCoreDBFirst
 {
-	app.UseExceptionHandler("/Home/Error");
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+			// Add services to the container.
+			builder.Services.AddControllersWithViews();
+			//Adding DBContext Service
+			builder.Services.AddDbContext<CompanyContext>(options =>
+			options.UseSqlServer(builder.Configuration.GetConnectionString("DBConnection")));
+
+			// Add services to the container.
+			builder.Services.AddControllersWithViews();
+
+			var app = builder.Build();
+
+			// Configure the HTTP request pipeline.
+			if (!app.Environment.IsDevelopment())
+			{
+				app.UseExceptionHandler("/Home/Error");
+			}
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			app.MapControllerRoute(
+				name: "default",
+				pattern: "{controller=Home}/{action=Index}/{id?}");
+
+			app.Run();
+		}
+	}
 }
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
